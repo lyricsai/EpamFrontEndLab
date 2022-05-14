5838dd982039f5f2b49ffec957b12648570f94d5
Anastasiya Litvinenka <loffy@rambler.ru>
2022-05-14 11:28:44 +0300

    js06

--- js06/script.js
+++ js06/script.js
@@ -18,13 +18,21 @@ class Stuff extends Entity {
 }
 
 class Box extends Entity {
+    stuff = [];
 
 }
 
 class User extends Entity {
-
-
+    box = [];
 }
 
-let user = new User('Pete');
-let box = new Box('Big box');
+const user1 = new User('Pete');
+const box1 = new Box('Big box');
+const stuff1 = new Stuff('something');
+const stuff2 = new Stuff('anything');
+
+box1.stuff.push(stuff1, stuff2);
+
+user1.box.push(box1);
+
+console.log(user1);
